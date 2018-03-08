56260183340351e8b444cd5b0f49acbccb44e3cc
markpet49 <markpet@yahoo.com>
2018-03-08 14:11:52 -0600

    Hyperlink example

---
+++ examples/HyperScreenText.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015-2017 WorldWind Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+requirejs(['./WorldWindShim',
+        './LayerManager'],
+    function (WorldWind,
+              LayerManager) {
+        "use strict";
+
+        // Tell WorldWind to log only warnings.
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        // Create the WorldWindow.
+        var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        /**
+         * Added imagery layers.
+         */
+        var layers = [
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        var hyperlinkCredit,
+            textAttributes = new WorldWind.TextAttributes(null),
+            textLayer = new WorldWind.RenderableLayer("Screen Text");
+
+        textAttributes.color = WorldWind.Color.MEDIUM_GRAY;
+
+        hyperlinkCredit = new WorldWind.ScreenText(
+            new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 1, WorldWind.OFFSET_FRACTION, 0.05), "NASA Worldind (https://worldwind.arc.nasa.gov)");
+        textAttributes = new WorldWind.TextAttributes(textAttributes);
+        textAttributes.offset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 1, WorldWind.OFFSET_FRACTION, 0);
+        textAttributes.enableOutline=false;
+        hyperlinkCredit.attributes = textAttributes;
+        hyperlinkCredit.userProperties.url="https://worldwind.arc.nasa.gov";
+        hyperlinkCredit.pickDelegate
+        textLayer.addRenderable(hyperlinkCredit);
+
+        wwd.addLayer(textLayer);
+
+        var layerManager = new LayerManager(wwd);
+
+        var handlePick = (function (o) {
+            var pickPoint = wwd.canvasCoordinates(o.clientX, o.clientY);
+
+            var pickList = wwd.pick(pickPoint);
+            if (pickList.objects.length > 0) {
+                for (var p = 0; p < pickList.objects.length; p++) {
+                    var pickedObject = pickList.objects[p];
+                    if (!pickedObject.isTerrain) {
+                        if (pickedObject.userObject instanceof WorldWind.ScreenText) {
+                            console.log(pickedObject.userObject.text);
+                        }
+                    }
+                }
+            }
+        }).bind(this);
+
+        // Listen for mouse moves and highlight text that the cursor rolls over.
+        wwd.addEventListener("mousemove", handlePick);
+
+        // Listen for taps on mobile devices and highlight text that the user taps.
+        var tapRecognizer = new WorldWind.TapRecognizer(wwd, handlePick);
+    });
